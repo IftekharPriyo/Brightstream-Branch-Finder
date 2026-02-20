d0b117b4d1dc682b3fa3ae702e906414c41c9dd3
Iftekhar Priyo <priyo.mobin123@gmail.com>
2026-02-20 13:28:56 +0600

    refactored

--- brightstream-branch-finder/src/components/NearestBranchWidget/IntroSection.tsx
+++ brightstream-branch-finder/src/components/NearestBranchWidget/IntroSection.tsx
@@ -6,7 +6,7 @@ export default function IntroSection(props: IntroSectionProps) {
   return (
     <>
       <p className="nbw-intro-text ease-up">
-        Find the nearest branch using GPS, or search branches by city.
+        Find the nearest branch from your location using GPS
       </p>
       <button onClick={props.onFindNearest} className="nbw-action-btn ease-up">
         Find Nearest Branch
